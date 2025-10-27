aec4f965d16202b6bfd4e55e9eb80190d59d0dfe
Emil Klitgaard <101024047+EmilKlitgaard@users.noreply.github.com>
2025-10-27 11:57:06 +0100

    Updated config integration (#20)

--- config.py
+++ config.py
@@ -1,25 +1,18 @@
-# MQTT Config
-SERVER = "10.67.156.254"
+import yaml
 
-# Whisper config
-SAMPLE_RATE = 16000           # Whisper expects 16 kHz
-HIGHPASS_HZ = 120.0           # High-pass filter cutoff
-LOWPASS_HZ = 5000.0           # Low-pass filter cutoff
-CHUNK_SECONDS = 2             # how often we trigger a partial transcription
-BUFFER_SECONDS = 4            # rolling context length
+class Config:
+    # Import YAML variables on objct init
+    def __init__(self) -> None:
+        with open('config.yaml', 'r') as file:
+            self.config = yaml.safe_load(file)
 
-MODEL_NAME = "tiny"       # change to "base" for lighter, "medium"/"large" for better accuracy (slower)
-MODEL_DEVICE = "cpu"      # set input device index or leave None for default
-AUDIO_DEVICE = None
-
-ERROR_WORDS = {"Thank you.", 
-               "Thanks for watching!"}  # Words to filter out from transcription
-
-DEFAULT_DISTANCE_CM = 100     # 100 cm if distance not given
-DEFAULT_TURN_DEG = 90         # 90 deg if angle not given
-MAX_TOKENS = 10               # cap transcription token buffer to at most 10 tokens
-
-# Deferral counters used by logic.handle_transcription
-PAUSE_ITTERATIONS = 2
-CURRENT_PAUSE = 0
-
+        # Go through all variables in self.config and set them as attributes
+        for section, params in self.config.items():
+            if isinstance(params, dict):
+                for var, value in params.items():
+                    setattr(self, var, value)
+                    if self.DEBUG: print(f"Set attribute: {var} = {value}")
+            else:
+                setattr(self, section, params)
+                if self.DEBUG: print(f"Set attribute: {section} = {params}")
+        if self.DEBUG: print(f"[Config class initialized]")
