9b45af78ab158751217f39e663d0c357e21dc71c
Emil Klitgaard <Ek@trailfinders.dk>
2025-10-29 13:01:37 +0100

    New branch

--- turtle_ros/tof_sensor.py
+++ turtle_ros/tof_sensor.py
@@ -1,27 +1,27 @@
-import time
-import RPi.GPIO as GPIO
 import threading as thread
+import RPi.GPIO as GPIO
+import time
+
+class Matrix:
+    def __init__(self):
+        self.matrix = [[0.0 for _ in range(16)] for _ in range(16)]
+        print("Custom struct 'dataStruct' initialized.")
+        print(self.matrix)
 
 class TOF_Sensor:
     # Variables
     STOP_THRESHOLD = 0.5  # distance in meters to trigger stop
-    LED_PIN = 18  # BCM pin 18 (physical pin 12)
-
-    # add custom struct: 16x16 matrix variable. The sensor data will be stored in this matrix.
+    LED_PIN = 12  # BCM pin 18 (physical pin 12)
 
     def __init__(self):
         # initialize the sensor first then start a new thread for detecting objects
 
-        GPIO.setmode(GPIO.BCM)    # or GPIO.BOARD for physical numbering
+        self.matrix = Matrix()
+
+        GPIO.setmode(GPIO.BOARD)    # or GPIO.BOARD for physical numbering
         GPIO.setup(self.LED_PIN, GPIO.OUT)
 
-<<<<<<< Updated upstream
         self.blink_led()
-=======
-
-    def make_roi(top, left, bottom, right): 
-
->>>>>>> Stashed changes
 
         self.floor_distance = self.get_floor_distance()
         thread.Thread(target = self.object_detection).start()
@@ -31,7 +31,7 @@ class TOF_Sensor:
         data = self.get_data()
         return data[15][8]  # assuming the floor distance is at the center of the last row
 
-    def get_data(self) -> matrix<16,16>:
+    def get_data(self) -> Matrix:
         # return the current sensor data as a 16x16 matrix
         pass
 
@@ -42,9 +42,9 @@ class TOF_Sensor:
     def blink_led(self):
         try:
             while True:
-                GPIO.output(LED_PIN, GPIO.HIGH)
+                GPIO.output(self.LED_PIN, GPIO.HIGH)
                 time.sleep(0.5)
-                GPIO.output(LED_PIN, GPIO.LOW)
+                GPIO.output(self.LED_PIN, GPIO.LOW)
                 time.sleep(0.5)
         except KeyboardInterrupt:
             pass
@@ -61,3 +61,8 @@ class TOF_Sensor:
             for value in data: #check floor distance
                 if value < self.floor_distance - 0.1: #if an object is detected closer than the floor distance minus a small margin
                     self.stop_robot()
+
+if __name__ == "__main__":
+    print("Initializing TOF Sensor...")
+    sensor = TOF_Sensor()
+    print("TOF Sensor initialized and running.")
